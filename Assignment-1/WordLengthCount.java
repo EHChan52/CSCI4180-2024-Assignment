2823b0123662e69f4d8f482a406f361abbdbd1f4
EHChan52 <heniy5218@gmail.com>
2024-10-10 23:51:58 +0800

    modified:   Assignment-1/WordLengthCount.java

--- Assignment-1/WordLengthCount.java
+++ Assignment-1/WordLengthCount.java
@@ -14,45 +14,44 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 public class WordLengthCount {
 
     public static class TokenizerMapper
-            extends Mapper<Object, Text, Text, IntWritable>{
+            extends Mapper<Object, Text, IntWritable, IntWritable>{
 
             private final static IntWritable one = new IntWritable(1);
-            private Text word = new Text();
+            private IntWritable wordLength = new IntWritable();
 
-            public void map(Object key, Text value, Context context
-                    ) throws IOException, InterruptedException {
+            public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+
                 StringTokenizer itr = new StringTokenizer(value.toString());
                 while (itr.hasMoreTokens()) {
-                    word.set(itr.nextToken());
-                    context.write(word, one);
+                    String word = itr.nextToken();
+                    wordLength.set(word.length());
+                    context.write(wordLength, one);
                 }
-                    }
+            }
     }
 
     public static class IntSumReducer
-            extends Reducer<Text,IntWritable,Text,IntWritable> {
+            extends Reducer<IntWritable, IntWritable, IntWritable, IntWritable> {
             private IntWritable result = new IntWritable();
 
-            public void reduce(Text key, Iterable<IntWritable> values,
-                    Context context
-                    ) throws IOException, InterruptedException {
+            public void reduce(IntWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
                 int sum = 0;
                 for (IntWritable val : values) {
                     sum += val.get();
                 }
                 result.set(sum);
                 context.write(key, result);
-                    }
+            }
     }
 
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf, "word count");
-        job.setJarByClass(WordCount.class);
+        Job job = Job.getInstance(conf, "word length count");
+        job.setJarByClass(WordLengthCount.class);
         job.setMapperClass(TokenizerMapper.class);
         job.setCombinerClass(IntSumReducer.class);
         job.setReducerClass(IntSumReducer.class);
-        job.setOutputKeyClass(Text.class);
+        job.setOutputKeyClass(IntWritable.class);
         job.setOutputValueClass(IntWritable.class);
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
