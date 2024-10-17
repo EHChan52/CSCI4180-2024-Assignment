e0916b9c696cf35faea0baa32c310bf865ed3d7e
chengkyf <chengkyf@gmail.com>
2024-10-17 18:05:14 +0800

    Stripe Version 1.3

--- Assignment-1/NgramRF.java
+++ Assignment-1/NgramRF.java
@@ -1,8 +1,6 @@
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.ArrayList;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.FloatWritable;
@@ -18,68 +16,60 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class NgramRF {
 
-    public static class InMapperCombiningNgramMapper extends Mapper<Object, Text, Text, MapWritable> {
+    public static class NgramMapper extends Mapper<Object, Text, Text, MapWritable> {
         private int N;
-        private Map<Text, MapWritable> intermediateMap;
+        private final Text prefixText = new Text();
+        private final MapWritable stripe = new MapWritable();
 
         @Override
         protected void setup(Context context) throws IOException, InterruptedException {
             Configuration conf = context.getConfiguration();
             N = Integer.parseInt(conf.get("N"));
-            intermediateMap = new HashMap<>();
         }
 
         @Override
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            String line = value.toString().replaceAll("['.]", " ");
-            String[] tokensArray = line.split("[^a-zA-Z0-9]+");
-            ArrayList<String> tokens = new ArrayList<>();
+            String line = value.toString().replaceAll("[^a-zA-Z0-9]+", " ");
+            String[] tokensArray = line.split("\\s+");
 
-            // Extract tokens, removing non-alphabetic characters
-            for (String token : tokensArray) {
-                if (!token.isEmpty()) {
-                    tokens.add(token);
-                }
-            }
-
-            // Iterate over the tokens to create n-grams
-            for (int i = 0; i <= tokens.size() - N; i++) {
+            for (int i = 0; i <= tokensArray.length - N; i++) {
                 StringBuilder ngramBuilder = new StringBuilder();
                 for (int j = 1; j < N; j++) {
                     if (j > 1) {
                         ngramBuilder.append(" ");
                     }
-                    ngramBuilder.append(tokens.get(i + j));
+                    ngramBuilder.append(tokensArray[i + j]);
                 }
 
-                String prefix = tokens.get(i);
+                String prefix = tokensArray[i];
                 String followingWords = ngramBuilder.toString();
 
-                // Update the stripe for in-mapper combining
-                Text prefixText = new Text(prefix);
-                MapWritable stripe = intermediateMap.getOrDefault(prefixText, new MapWritable());
-                Text followingWordsText = new Text(followingWords);
-                
-                IntWritable count = (IntWritable) stripe.getOrDefault(followingWordsText, new IntWritable(0));
-                count.set(count.get() + 1);
-                stripe.put(followingWordsText, count);
+                prefixText.set(prefix);
+                stripe.clear();
+                stripe.put(new Text(followingWords), new IntWritable(1));
+                stripe.put(new Text("*"), new IntWritable(1));
 
-                // Update the count for "*"
-                IntWritable totalCount = (IntWritable) stripe.getOrDefault(new Text("*"), new IntWritable(0));
-                totalCount.set(totalCount.get() + 1);
-                stripe.put(new Text("*"), totalCount);
-
-                // Update the intermediate map
-                intermediateMap.put(prefixText, stripe);
+                context.write(prefixText, stripe);
             }
         }
+    }
 
+    public static class NgramCombiner extends Reducer<Text, MapWritable, Text, MapWritable> {
         @Override
-        protected void cleanup(Context context) throws IOException, InterruptedException {
-            // Emit all combined intermediate results
-            for (Map.Entry<Text, MapWritable> entry : intermediateMap.entrySet()) {
-                context.write(entry.getKey(), entry.getValue());
+        public void reduce(Text key, Iterable<MapWritable> values, Context context) throws IOException, InterruptedException {
+            MapWritable combinedStripe = new MapWritable();
+
+            for (MapWritable stripe : values) {
+                for (Map.Entry<Writable, Writable> entry : stripe.entrySet()) {
+                    Text followingWords = (Text) entry.getKey();
+                    IntWritable value = (IntWritable) entry.getValue();
+                    IntWritable existingValue = (IntWritable) combinedStripe.getOrDefault(followingWords, new IntWritable(0));
+                    existingValue.set(existingValue.get() + value.get());
+                    combinedStripe.put(followingWords, existingValue);
+                }
             }
+
+            context.write(key, combinedStripe);
         }
     }
 
@@ -98,30 +88,28 @@ public class NgramRF {
             MapWritable aggregateStripe = new MapWritable();
             int totalCount = 0;
 
-            // Aggregate all stripes
             for (MapWritable stripe : values) {
                 for (Map.Entry<Writable, Writable> entry : stripe.entrySet()) {
-                    Text nextWord = (Text) entry.getKey();
-                    IntWritable count = (IntWritable) entry.getValue();
-                    if (nextWord.toString().equals("*")) {
-                        totalCount += count.get();
-                    } else {
-                        IntWritable currentCount = (IntWritable) aggregateStripe.getOrDefault(nextWord, new IntWritable(0));
-                        currentCount.set(currentCount.get() + count.get());
-                        aggregateStripe.put(nextWord, currentCount);
-                    }
+                    Text followingWords = (Text) entry.getKey();
+                    IntWritable value = (IntWritable) entry.getValue();
+                    IntWritable existingValue = (IntWritable) aggregateStripe.getOrDefault(followingWords, new IntWritable(0));
+                    existingValue.set(existingValue.get() + value.get());
+                    aggregateStripe.put(followingWords, existingValue);
                 }
             }
 
-            // Calculate relative frequencies and emit results if frequency >= theta
+            totalCount = ((IntWritable) aggregateStripe.get(new Text("*"))).get();
+
             for (Map.Entry<Writable, Writable> entry : aggregateStripe.entrySet()) {
                 Text nextWord = (Text) entry.getKey();
-                int count = ((IntWritable) entry.getValue()).get();
-                float frequency = (float) count / totalCount;
+                if (!nextWord.toString().equals("*")) {
+                    int count = ((IntWritable) entry.getValue()).get();
+                    float frequency = (float) count / totalCount;
 
-                if (frequency >= theta) {
-                    relativeFrequency.set(frequency);
-                    context.write(new Text(key.toString() + " " + nextWord.toString()), relativeFrequency);
+                    if (frequency >= theta) {
+                        relativeFrequency.set(frequency);
+                        context.write(new Text(key.toString() + " " + nextWord.toString()), relativeFrequency);
+                    }
                 }
             }
         }
@@ -144,25 +132,25 @@ public class NgramRF {
         Configuration conf = new Configuration();
         conf.set("N", args[2]);
         conf.set("theta", args[3]);
-    
+        conf.set("mapreduce.output.textoutputformat.separator", " ");
         Job job = Job.getInstance(conf, "N-gram Relative Frequency");
         job.setJarByClass(NgramRF.class);
 
-        // Set Mapper and Reducer classes
-        job.setMapperClass(InMapperCombiningNgramMapper.class);
+        job.setMapperClass(NgramMapper.class);
+        job.setCombinerClass(NgramCombiner.class);
         job.setReducerClass(RelativeFrequencyReducer.class);
 
-        // Set key/value classes for Mapper and Reducer outputs
         job.setMapOutputKeyClass(Text.class);
         job.setMapOutputValueClass(MapWritable.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(FloatWritable.class);
-    
+
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
-    
+
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
+
 
 
