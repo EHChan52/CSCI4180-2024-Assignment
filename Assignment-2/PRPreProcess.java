0d1c2866a01817c6009f3fc4c3cdb3ea7403f18a
Withercola <126001690+EHChan52@users.noreply.github.com>
2024-11-10 17:10:15 +0800

    modified

--- Assignment-2/PRPreProcess.java
+++ Assignment-2/PRPreProcess.java
@@ -1,1 +1,69 @@
 package assg2;
+
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import java.io.IOException;
+
+public class PRPreProcess {
+    public class Graph{
+        private Map<Integer, List<Integer> > adjacencyList;
+
+        public Graph() {
+            adjacencyList = new HashMap<>();
+        }
+
+        public void addVertex(int vertex){
+            adjacencyList.put(vertex, new ArrayList<>());
+        }
+
+        public void addEdge(int source, int destination){
+            adjacencyList.get(source).add(destination);
+        }
+        
+        public void removeVertex(int vertex){
+            adjacencyList.remove(vertex);
+            for (List<Integer> neighbors :
+                adjacencyList.values()) {
+                neighbors.remove(Integer.valueOf(vertex));
+            }
+        }
+
+        public void removeEdge(int source, int destination){
+            adjacencyList.get(source).remove(
+                Integer.valueOf(destination));
+        }
+        
+    }
+
+    public class PreprocessMapper extends Mapper<>{
+
+    }
+
+    public class PreprocessReducer extends Reducer<>{
+        
+    }
+
+    public static void main(String fileName) throws Exception {
+        Graph adjList = new Graph();
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "PRProcess");
+        job.setJarByClass(PRPreProcess.class);
+        job.setMapperClass(PreprocessMapper.class);
+        job.setReducerClass(PreprocessReducer.class);
+
+        job.setOutputKeyClass(IntWritable.class);
+        //job.setOutputValueClass(ArrayWritable.class);
+
+        FileInputFormat.addInputPath(job, new Path(fileName));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
--- Assignment-2/PageRank.java
+++ Assignment-2/PageRank.java
@@ -1,1 +1,26 @@
 package assg2;
+
+public class PageRank{
+    public static void main(String[] args) throws Exception {
+        if (args.length < 5) {
+            System.err.println("Usage: hadoop jar [.jar file] PageRank [alpha] [iteration] [threshold] [infile] [outdir]");
+            System.exit(-1);
+        }
+
+        float alpha = Float.parseFloat(args[0]);
+        int iteration = Integer.parseInt(args[1]);
+        float threshold = Float.parseFloat(args[2]);
+        
+        Configuration conf = new Configuration();
+        conf.set("alpha", args[0]);
+        conf.set("iteration", args[1]);
+        conf.set("threshold", args[2]);
+
+        
+
+        FileInputFormat.addInputPath(job, new Path(args[3]));
+        FileOutputFormat.setOutputPath(job, new Path(args[4]));
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
