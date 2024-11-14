468112ff1aee2c16f9950bbc634122f433637534
EHChan52 <heniy5218@gmail.com>
2024-11-14 16:55:53 +0800

    new file:   Assignment-2/1

---
+++ Assignment-2/PRAdjust copy.java
@@ -0,0 +1,78 @@
+package assg2p2;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+
+import java.io.IOException;
+
+public class PRAdjust {
+    public static class PRAdjustMapper extends Mapper<IntWritable, PRNodeWritable, IntWritable, PRNodeWritable> {
+        private float alpha;
+        private float threshold;
+        private int numNodes;
+
+        @Override
+        protected void setup(Context context) throws IOException, InterruptedException {
+            Configuration conf = context.getConfiguration();
+            alpha = conf.getFloat("alpha", 0.15f);
+            threshold = conf.getFloat("threshold", 0.001f);
+            numNodes = conf.getInt("numNodes", 1);
+        }
+
+        @Override
+        public void map(IntWritable key, PRNodeWritable value, Context context) throws IOException, InterruptedException {
+            double sum = value.getPageRankValue().get();
+            double missingMass = context.getConfiguration().getDouble("missingMass", 0.0);
+            double adjustedRank = alpha / numNodes + (1 - alpha) * (sum + missingMass / numNodes);
+
+            value.setPageRankValue(new DoubleWritable(adjustedRank));
+            context.write(key, value);
+        }
+    }
+
+    public static class PRAdjustReducer extends Reducer<IntWritable, PRNodeWritable, IntWritable, PRNodeWritable> {
+        @Override
+        public void reduce(IntWritable key, Iterable<PRNodeWritable> values, Context context) throws IOException, InterruptedException {
+            PRNodeWritable adjustedNode = new PRNodeWritable();
+            double adjustedRank = 0.0;
+            for (PRNodeWritable val : values) {
+                adjustedRank += val.getPageRankValue().get();
+                adjustedNode.setNodeID(val.getNodeID());
+                adjustedNode.setWholeAdjList(val.getWholeAdjList());
+            }
+            adjustedNode.setPageRankValue(new DoubleWritable(adjustedRank));
+            context.write(key, adjustedNode);
+        }
+    }
+
+    public static Job getPRAdjustJob(Configuration conf, Path inputPath, Path outputPath) throws IOException {
+        Job prAdjustJob = Job.getInstance(conf, "PRAdjust");
+
+        prAdjustJob.setJarByClass(PRAdjust.class);
+        prAdjustJob.setMapperClass(PRAdjustMapper.class);
+        prAdjustJob.setReducerClass(PRAdjustReducer.class);
+
+        prAdjustJob.setMapOutputKeyClass(IntWritable.class);
+        prAdjustJob.setMapOutputValueClass(PRNodeWritable.class);
+
+        prAdjustJob.setOutputKeyClass(IntWritable.class);
+        prAdjustJob.setOutputValueClass(PRNodeWritable.class);
+
+        prAdjustJob.setInputFormatClass(SequenceFileInputFormat.class);
+        prAdjustJob.setOutputFormatClass(SequenceFileOutputFormat.class);
+
+        FileInputFormat.addInputPath(prAdjustJob, inputPath);
+        FileOutputFormat.setOutputPath(prAdjustJob, outputPath);
+
+        return prAdjustJob;
+    }
+}
---
+++ Assignment-2/PRNodeWritable copy.java
@@ -0,0 +1,106 @@
+package assg2p2;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.MapWritable;
+import org.apache.hadoop.io.BooleanWritable;
+
+public class PRNodeWritable implements Writable {
+    private IntWritable nodeID;
+    private MapWritable adjList;
+    private DoubleWritable pageRankValue;
+    private BooleanWritable pageRankValueFixed;
+
+    public PRNodeWritable(){
+        this.nodeID = new IntWritable();
+        this.adjList = new MapWritable();
+        this.pageRankValue = new DoubleWritable(Float.NEGATIVE_INFINITY);
+        this.pageRankValueFixed = new BooleanWritable(false);
+    }
+
+    public PRNodeWritable(IntWritable nodeID, DoubleWritable pageRankValue) {
+        this.nodeID = nodeID;
+        this.adjList = new MapWritable();
+        this.pageRankValue = pageRankValue;
+        this.pageRankValueFixed = new BooleanWritable(false);
+    }
+
+    public void setNodeID(IntWritable nodeID) {
+        this.nodeID = nodeID;
+    }
+
+    public IntWritable getNodeID() {
+        return this.nodeID;
+    }
+
+    public void setPageRankValue(DoubleWritable pageRankValue) {
+        this.pageRankValue = pageRankValue;
+    }
+
+    public DoubleWritable getPageRankValue() {
+        return this.pageRankValue;
+    }
+
+    public void setPageRankValueFixed(BooleanWritable pageRankValueFixed) {
+        this.pageRankValueFixed = pageRankValueFixed;
+    }
+
+    public BooleanWritable getPageRankValueFixed() {
+        return this.pageRankValueFixed;
+    }
+
+    public void setWholeAdjList(MapWritable adjList) {
+        this.adjList = adjList;
+    }
+
+    public MapWritable getWholeAdjList() {
+        return this.adjList;
+    }
+
+    public BooleanWritable isNode() {
+        return new BooleanWritable(this.adjList.size() > 0);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder adjListStr = new StringBuilder();
+        for (Writable k : adjList.keySet()) {
+            if (adjListStr.length() > 0) {
+                adjListStr.append(" ");
+            }
+            adjListStr.append(((IntWritable) k).get());
+        }
+        return this.nodeID + " " + this.pageRankValue + " " + adjListStr.toString();
+    }
+
+    public static PRNodeWritable fromString(String str) {
+        String[] parts = str.split(" ");
+        PRNodeWritable prNode = new PRNodeWritable();
+        prNode.setNodeID(new IntWritable(Integer.parseInt(parts[0])));
+        prNode.setPageRankValue(new DoubleWritable(Double.parseDouble(parts[1])));
+        for (int i = 2; i < parts.length; i++) {
+            prNode.adjList.put(new IntWritable(Integer.parseInt(parts[i])), new DoubleWritable(1.0));
+        }
+        return prNode;
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException {
+        nodeID.readFields(in);
+        adjList.readFields(in);
+        pageRankValue.readFields(in);
+        pageRankValueFixed.readFields(in);
+    }
+
+    @Override
+    public void write(DataOutput out) throws IOException {
+        nodeID.write(out);
+        adjList.write(out);
+        pageRankValue.write(out);
+        pageRankValueFixed.write(out);
+    }
+}
