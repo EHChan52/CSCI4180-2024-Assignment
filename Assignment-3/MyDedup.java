aceb1119c79e3cfce95f23e5794dcae48941043e
EHChan52 <heniy5218@gmail.com>
2024-12-09 00:47:14 +0800

    feat: add file creation logic for mydedup.index in MyDedup

--- Assignment-3/MyDedup.java
+++ Assignment-3/MyDedup.java
@@ -1,5 +1,25 @@
+import java.io.File;
+import java.io.IOException;
+
 class MyDedup {
     public static void main(String[] args) {
+
+        // Create mydedup.data file if it does not exist
+        File indexFile = new File("mydedup.index");
+        if (!indexFile.exists()) {
+            try {
+                if (indexFile.createNewFile()) {
+                    System.out.println("mydedup.index file created.");
+                } else {
+                    System.out.println("Failed to create mydedup.index file.");
+                }
+            } catch (IOException e) {
+                System.out.println("An error occurred while creating mydedup.index file.");
+            }
+        } else {
+            System.out.println("mydedup.index file already exists.");
+        }
+
 
         if("upload".equals(args[0])){
             System.out.println("upload");
