4cea61a22577fe318a449d7b7bf66c6422f96e41
EHChan52 <heniy5218@gmail.com>
2024-12-08 22:41:46 +0800

    feat: add argument handling and debug output in MyDedup

--- Assignment-3/MyDedup.java
+++ Assignment-3/MyDedup.java
@@ -1,5 +1,17 @@
 class MyDedup {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+
+        if("upload".equals(args[0])){
+            System.out.println("upload");
+        }
+        else if("download".equals(args[0])){
+            System.out.println("download");
+        }
+
+        //debug statement
+        System.out.println("Number of arguments: " + args.length);
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("Argument " + (i + 1) + ": " + args[i]);
+        }
     }
 }
