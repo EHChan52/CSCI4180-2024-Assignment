64fc5685fd8ff928c9cf41cf9263265aa2a69d20
EHChan52 <heniy52182@gmail.com>
2024-12-11 20:46:25 +0800

    seperate chunk storage to 2 different codes

---
+++ Assignment-3/WrapToContainer.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class WrapToContainer {
+    private static final int CONTAINER_SIZE = 1024 * 1024;
+    
+    public ArrayList<Container> createContainers() {
+        ArrayList<Container> containerList = new ArrayList<>();
+        
+        
+        
+        return containerList;
+    }
+}
