3e363bbf7c0104e877aaf9334db345d9a4f712ef
chengkyf <chengkyf@gmail.com>
2024-12-12 23:08:55 +0800

    Fix

--- Assignment-3/WrapToContainer.java
+++ Assignment-3/WrapToContainer.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class WrapToContainer {
     private static final long CONTAINER_SIZE = 1024 * 1024;
-    long id = 0;
+    Integer id = 0;
 
     public ArrayList<Container> createContainers(ArrayList<Chunk> chunksList) {
         ArrayList<Container> containerList = new ArrayList<>();
